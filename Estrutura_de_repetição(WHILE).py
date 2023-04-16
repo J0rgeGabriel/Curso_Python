c60ada37a69f2bc08edd9fb8f9e77bebe6ac0b27
Jorge Gabriel <127042883+J0rgeGabriel@users.noreply.github.com>
2023-04-16 15:58:04 -0300

    Exercicio 02 feito

--- Estrutura_de_repetição(WHILE).py
+++ Estrutura_de_repetição(WHILE).py
@@ -40,3 +40,48 @@ while sexo not in ("M/F"):
     sexo = input("Sexo invalido, digite novamente o seu sexo [M/F]: ").upper()
 print(f"Sexo registrado: {sexo}")
 '''
+'''
+while True:     #o comando While True: é uma estrutura de repetição que cria um loop infinito.
+    print("Loop infinito!")
+'''
+'''
+while True:
+    for x in range(1,6):
+        print(x)
+'''
+
+while True:
+    n1 = float(input("Digite o primeiro numero: "))
+    n2 = float(input("Digite o segundo numero: "))
+    opcao = input("Escolha uma opção:\n"
+                  "1 - Somar\n"
+                  "2 - multiplicar\n"
+                  "3 - maior \n"
+                  "4 - novos numeros\n"
+                  "5 - sair do programa")
+
+    if opcao == "1":
+        total = n1 + n2
+        print(f"A soma do primeiro numero + o segundo numero é {total}")
+
+    elif opcao == "2":
+        total = n1 * n2
+        print(f"A multiplicação do primeiro numero com o segundo numero é {total}")
+
+    elif opcao == "3":
+        if n1 > n2:
+            print(f"O numero {n1} é maior que {n2}")
+        else:
+            print(f"O numero {n2} é maior que {n1}")
+
+    elif opcao == "4":
+        continue #volta ao inicio do loop
+
+    elif opcao == "5":
+        print("Saindo do programa")
+        break
+
+    else:
+        print("Opção inválida, tente novamente.")
+print("\n")
+print("Fim do programa.")
