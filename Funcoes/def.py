16af4d269ad3ccecc831b032945b0733ab78a4e9
Jorge Gabriel <127042883+J0rgeGabriel@users.noreply.github.com>
2023-07-01 16:33:15 -0300

    ...

--- Funcoes/def.py
+++ Funcoes/def.py
@@ -13,7 +13,6 @@ python()
 def imprimir(a, b, c):
     return max(a, b, c)
 
-
 print(imprimir(1, 2, 3))
 print(imprimir(10, 20, 30))
 print(imprimir(100, 200, 300))
@@ -48,3 +47,16 @@ def soma(x, y, z):
 soma(1, 2, 3)
 soma(10, 20, 30)
 soma(100, 200, 300)
+
+
+def soma(x, y, z = None):
+    if z is not None:
+        print(f"{x=} + {y=} + {z=} = {x + y + z}")
+    else:
+        print(f"{x=} + {y=} = {x + y}")
+
+
+soma(1, 2)
+soma(10, 20)
+soma(100, 200)
+soma(1, 2, 3)
